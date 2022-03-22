c38c4c414302af812092ca5972751872056e66f7
EveCharbie <eve.charbonneau.1@umontreal.ca>
2022-03-22 14:39:09 -0400

    fixed heatmap

--- rectangle_heatmap/create_gaussian_heatMap.py
+++ rectangle_heatmap/create_gaussian_heatMap.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 import cv2
+import pickle
 
 
 def points_to_gaussian_heatmap(centers, height, width, scale):
     # from : https://stackoverflow.com/questions/44945111/how-to-efficiently-compute-the-heat-map-of-two-gaussian-distribution-in-python
 
     gaussians = []
-    for y, x in centers:
+    for x, y in centers:
         s = np.eye(2) * scale
         g = multivariate_normal(mean=(x, y), cov=s)
         gaussians.append(g)
@@ -29,7 +30,7 @@ def points_to_gaussian_heatmap(centers, height, width, scale):
 
 
 image_width = 214
-image_heighgt = 428
+image_height = 428
 gaussian_width = 50
 
 plt.figure()
@@ -47,18 +48,21 @@ plt.plot(np.array([53, 161]), np.array([268, 268]), '-k', linewidth=1)
 plt.plot(np.array([107-25, 107+25]), np.array([214, 214]), '-k', linewidth=1)
 plt.plot(np.array([107, 107]), np.array([214-25, 214+25]), '-k', linewidth=1)
 
-gaze_position_labels = f'../output/{movie_name[:-4]}_labeling_points.pkl'
-out_path = '/home/user/Documents/Programmation/Eve/rectangle-labelling/rectangle-labelling/output/Results/'
+movie_path = "../output/"
+#########################################################################################
+movie_name = "df297219_0_0-43_588"  #"PI world v1 ps1" #
+gaze_position_labels = movie_path + movie_name + "_labeling_points.pkl"
+out_path = '/home/user/Documents/Programmation/rectangle-labelling/output/Results'
 subject_name = 'GuSe'
-move_names = ['4-o', '8--1o', '8--o', '8--1o']
+move_names = ['4-o'] # , '8--1o', '8--o', '8--1o']
 
 file = open(gaze_position_labels, "rb")
 points_labels, active_points, curent_AOI_label, csv_eye_tracking = pickle.load(file)
 
 zeros_clusters_index = curent_AOI_label["Not an acrobatics"][:-1] - curent_AOI_label["Not an acrobatics"][1:]
 zeros_clusters_index = np.hstack((0, zeros_clusters_index))
-start_of_move_index = np.where(zeros_clusters_index == -1)
-end_of_move_index = np.where(zeros_clusters_index == 1)
+end_of_move_index = np.where(zeros_clusters_index == -1)[0].tolist() # [np.where(curent_AOI_label["Wall front"] == 1)[0][0]] ###############3
+start_of_move_index = np.where(zeros_clusters_index == 1)[0].tolist() # [np.where(points_labels["0"][0, :] != 0)[0][-1]] ##############
 
 if len(move_names) != len(start_of_move_index) or len(move_names) != len(end_of_move_index):
     raise RuntimeError("Not the right number of skills!")
@@ -73,14 +77,18 @@ for i in range(len(move_names)):
     start = start_of_move_index[i]
     end = end_of_move_index[i]
     centers_gaze_bed_i = []
-    gaze_total_move = start - end
+    gaze_total_move = end - start
     number_of_trampoline_bed = 0
     number_of_wall_front = 0
     number_of_wall_back = 0
     number_of_ceiling = 0
+    number_of_trampoline = 0
     for j in range(start, end):
-        if curent_AOI_label["Trampoline bed"] == 1:
-            centers_gaze_bed_i.append((csv_eye_tracking[j, 5], csv_eye_tracking[j, 6]))
+        if curent_AOI_label["Trampoline bed"][j] == 1:
+            index_gaze = np.where(csv_eye_tracking[:, 4] == j)[0]
+            for k in index_gaze:
+                if csv_eye_tracking[k, 5] != 0 and csv_eye_tracking[k, 6] != 0:
+                    centers_gaze_bed_i.append((csv_eye_tracking[k, 5], csv_eye_tracking[k, 6]))
             number_of_trampoline_bed += 1
         elif curent_AOI_label["Wall front"][j] == 1:
             number_of_wall_front += 1
@@ -88,23 +96,29 @@ for i in range(len(move_names)):
             number_of_wall_back += 1
         elif curent_AOI_label["Ceiling"][j] == 1:
             number_of_ceiling += 1
+        elif curent_AOI_label["Trampoline"][j] == 1:
+            number_of_trampoline += 1
+
     centers_gaze_bed[i] = centers_gaze_bed_i
-    img = points_to_gaussian_heatmap(centers_gaze_bed, image_width, image_heighgt, gaussian_width)
+    img = points_to_gaussian_heatmap(centers_gaze_bed[i], image_height, image_width, gaussian_width)
     plt.imshow(img)
     plt.show()
 
-    move_summary[i] = {"centers": centers_gaze_bed_i,
+    move_summary[i] = {"movement name": move_names[i],
+                       "subject name": subject_name,
+                       "movie name": movie_name,
+                       "centers": centers_gaze_bed_i,
                        "heat map": img,
                        "trampoline bed proportions": number_of_trampoline_bed/gaze_total_move,
                        "wall front proportions": number_of_wall_front/gaze_total_move,
                        "wall back proportions": number_of_wall_back/gaze_total_move,
                        "ceiling proportions": number_of_ceiling/gaze_total_move}
 
-    with open(f'{out_path}/{subject_name}/{gaze_position_labels[:-20]}_heat_map_{i}.pkl', 'wb') as handle:
+    with open(f'{out_path}/{subject_name}/{move_names[i]}/{movie_name}_heat_map_{i}.pkl', 'wb') as handle:
         pickle.dump(move_summary[i], handle)
 
 
-with open(f'../output/{gaze_position_labels[:-20]}_heat_map.pkl', 'wb') as handle:
+with open(f'{gaze_position_labels[:-20]}_heat_map.pkl', 'wb') as handle:
     pickle.dump(move_summary, handle)
 
 
