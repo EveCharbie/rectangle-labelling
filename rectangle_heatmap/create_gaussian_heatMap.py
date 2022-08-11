7078b7fa304e12d3b053223c0b565cbe768b3b76
Charbie <eve.charbonneau.1@umontreal.ca>
2022-08-11 14:05:05 -0400

    switched to loop over every trial in gaze_trajectory_Pupiol_Xsens

--- rectangle_heatmap/create_gaussian_heatMap.py
+++ rectangle_heatmap/create_gaussian_heatMap.py
@@ -8,6 +8,78 @@ import os
 import pandas as pd
 
 
+def load_pupil(gaze_position_labels, eye_tracking_data_path):
+
+    file = open(gaze_position_labels, "rb")
+    points_labels, active_points, curent_AOI_label, csv_eye_tracking = pickle.load(file)
+
+    filename_gaze = eye_tracking_data_path + 'gaze.csv'
+    filename_timestamps = eye_tracking_data_path + 'world_timestamps.csv'
+    filename_info = eye_tracking_data_path + 'info.json'
+
+
+    csv_gaze_read = np.char.split(pd.read_csv(filename_gaze, sep='\t').values.astype('str'), sep=',')
+    timestamp_image_read = np.char.split(pd.read_csv(filename_timestamps, sep='\t').values.astype('str'), sep=',')
+    timestamp_image = np.zeros((len(timestamp_image_read, )))
+    for i in range(len(timestamp_image_read)):
+        timestamp_image[i] = timestamp_image_read[i][0][2]
+    info = np.char.split(pd.read_csv(filename_info, sep='\t').values.astype('str'), sep=',')
+    serial_number_str = info[15][0][0]
+    num_quote = 0
+    for pos, char in enumerate(serial_number_str):
+        if char == '"':
+            num_quote += 1
+            if num_quote == 3:
+                SCENE_CAMERA_SERIAL_NUMBER = serial_number_str[pos+1:pos+6]
+                break
+
+    csv_eye_tracking = np.zeros((len(csv_gaze_read), 7))
+    for i in range(len(csv_gaze_read)):
+        csv_eye_tracking[i, 0] = float(csv_gaze_read[i][0][2])  # timestemp
+        csv_eye_tracking[i, 1] = int(round(float(csv_gaze_read[i][0][3])))  # pos_x
+        csv_eye_tracking[i, 2] = int(round(float(csv_gaze_read[i][0][4])))  # pos_y
+        csv_eye_tracking[i, 4] = np.argmin(np.abs(csv_eye_tracking[i, 0] - timestamp_image))  # closest image timestemp
+
+    # embed()
+    # plt.figure()
+    # plt.plot(csv_eye_tracking[:, 0], '-b')
+    # plt.plot(timestamp_image, '-r')
+    # plt.show()
+
+
+    time_stamps_eye_tracking = np.zeros((len(timestamp_image),))
+    time_stamps_eye_tracking_index_on_pupil = np.zeros((len(timestamp_image),))
+    for i in range(len(timestamp_image)):
+        time_stamps_eye_tracking_index_on_pupil[i] = np.argmin(np.abs(csv_eye_tracking[:, 0] - float(timestamp_image[i])))
+
+
+    zeros_clusters_index = curent_AOI_label["Not an acrobatics"][:-1] - curent_AOI_label["Not an acrobatics"][1:]
+    zeros_clusters_index = np.hstack((0, zeros_clusters_index))
+
+    end_of_cluster_index_image = np.where(zeros_clusters_index == -1)[0].tolist()
+    start_of_cluster_index_image = np.where(zeros_clusters_index == 1)[0].tolist()
+
+    start_of_move_index_image = []
+    end_of_move_index_image = []
+    start_of_jump_index_image = []
+    end_of_jump_index_image = []
+    for i in range(len(start_of_cluster_index_image)):
+        if curent_AOI_label["Jump"][start_of_cluster_index_image[i] + 1] == 1:
+            start_of_jump_index_image += [start_of_cluster_index_image[i]]
+            end_of_jump_index_image += [end_of_cluster_index_image[i]]
+        else:
+            start_of_move_index_image += [start_of_cluster_index_image[i]]
+            end_of_move_index_image += [end_of_cluster_index_image[i]]
+
+
+    end_of_move_index = time_stamps_eye_tracking_index_on_pupil[end_of_move_index_image]
+    start_of_move_index = time_stamps_eye_tracking_index_on_pupil[start_of_move_index_image]
+    end_of_jump_index = time_stamps_eye_tracking_index_on_pupil[end_of_jump_index_image]
+    start_of_jump_index = time_stamps_eye_tracking_index_on_pupil[start_of_jump_index_image]
+
+    return curent_AOI_label, csv_eye_tracking, csv_eye_tracking, start_of_move_index, end_of_move_index, start_of_jump_index, end_of_jump_index
+
+
 def points_to_gaussian_heatmap(centers, height, width, scale):
     # from : https://stackoverflow.com/questions/44945111/how-to-efficiently-compute-the-heat-map-of-two-gaussian-distribution-in-python
 
@@ -45,52 +117,12 @@ def put_lines_on_fig():
     plt.plot(np.array([107, 107]), np.array([214 - 25, 214 + 25]), '-w', linewidth=1)
     return
 
+def run_create_heatmaps(move_names, start_of_move_index_image, end_of_move_index_image, curent_AOI_label, csv_eye_tracking):
+    image_width = 214
+    image_height = 428
+    gaussian_width = 50
 
-image_width = 214
-image_height = 428
-gaussian_width = 50
 
-csv_name = "/home/user/Documents/Programmation/rectangle-labelling/Trials_name_mapping.csv"
-csv_table = np.char.split(pd.read_csv(csv_name, sep='\t').values.astype('str'), sep=',')
-
-for i_trial in range(len(csv_table)):
-    if csv_table[i_trial][0][12] != 'True':
-        continue
-    movie_path = "/home/user/disk/Eye-tracking/PupilData/points_labeled/"
-    movie_name = csv_table[i_trial][0][7].replace('.', '_')
-    gaze_position_labels = movie_path + movie_name + "_labeling_points.pkl"
-    out_path = '/home/user/disk/Eye-tracking/Results'
-    subject_name = csv_table[i_trial][0][0]
-    move_names = csv_table[i_trial][0][1].split(" ")
-    repetition_number = csv_table[i_trial][0][2].split(" ")
-    move_orientation = [int(x) for x in csv_table[i_trial][0][3].split(" ")]
-    subject_expertise = csv_table[i_trial][0][9]
-
-    file = open(gaze_position_labels, "rb")
-    points_labels, active_points, curent_AOI_label, csv_eye_tracking = pickle.load(file)
-
-    zeros_clusters_index = curent_AOI_label["Not an acrobatics"][:-1] - curent_AOI_label["Not an acrobatics"][1:]
-    zeros_clusters_index = np.hstack((0, zeros_clusters_index))
-
-    end_of_cluster_index_image = np.where(zeros_clusters_index == -1)[0].tolist()
-    start_of_cluster_index_image = np.where(zeros_clusters_index == 1)[0].tolist()
-
-    start_of_move_index_image = []
-    end_of_move_index_image = []
-    start_of_jump_index_image = []
-    end_of_jump_index_image = []
-    for i in range(len(start_of_cluster_index_image)):
-        if curent_AOI_label["Jump"][start_of_cluster_index_image[i] + 1] == 1:
-            start_of_jump_index_image += [start_of_cluster_index_image[i]]
-            end_of_jump_index_image += [end_of_cluster_index_image[i]]
-        else:
-            start_of_move_index_image += [start_of_cluster_index_image[i]]
-            end_of_move_index_image += [end_of_cluster_index_image[i]]
-
-    # zeros_clusters_index = curent_AOI_label["Not an acrobatics"][:-1] - curent_AOI_label["Not an acrobatics"][1:]
-    # zeros_clusters_index = np.hstack((0, zeros_clusters_index))
-    # end_of_move_index = np.where(zeros_clusters_index == -1)[0].tolist()
-    # start_of_move_index = np.where(zeros_clusters_index == 1)[0].tolist()
 
     if len(move_names) != len(start_of_move_index_image) or len(move_names) != len(end_of_move_index_image):
         plt.figure()
@@ -174,3 +206,35 @@ for i_trial in range(len(csv_table)):
     with open(f'{gaze_position_labels[:-20]}_heat_map.pkl', 'wb') as handle:
         pickle.dump(move_summary, handle)
 
+
+def __main__():
+
+    if os.path.exists('/home/user'):
+        root_path = '/home/user'
+    elif os.path.exists('/home/fbailly'):
+        root_path = '/home/fbailly'
+    elif os.path.exists('/home/charbie'):
+        root_path = '/home/charbie'
+
+    csv_name = root_path + "/Documents/Programmation/rectangle-labelling/Trials_name_mapping.csv"
+    csv_table = np.char.split(pd.read_csv(csv_name, sep='\t').values.astype('str'), sep=',')
+
+    for i_trial in range(len(csv_table)):
+        if csv_table[i_trial][0][12] != 'True':
+            continue
+        movie_path = "/home/user/disk/Eye-tracking/PupilData/points_labeled/"
+        movie_name = csv_table[i_trial][0][7].replace('.', '_')
+        gaze_position_labels = movie_path + movie_name + "_labeling_points.pkl"
+        out_path = '/home/user/disk/Eye-tracking/Results'
+        subject_name = csv_table[i_trial][0][0]
+        move_names = csv_table[i_trial][0][1].split(" ")
+        repetition_number = csv_table[i_trial][0][2].split(" ")
+        move_orientation = [int(x) for x in csv_table[i_trial][0][3].split(" ")]
+        subject_expertise = csv_table[i_trial][0][9]
+
+        curent_AOI_label, csv_eye_tracking, csv_eye_tracking, start_of_move_index, end_of_move_index, start_of_jump_index, end_of_jump_index = load_pupil(
+            gaze_position_labels, eye_tracking_data_path)
+
+
+        run_create_heatmaps(move_names, start_of_move_index_image, end_of_move_index_image, curent_AOI_label, csv_eye_tracking)
+    
